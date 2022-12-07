28813508deb9df3526c520d036f50d54af57f366
davis122991 <110353699+davis122991@users.noreply.github.com>
2022-12-07 22:20:16 +0200

    Added user authentication and authorization

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,18 @@ const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
-connectDB();
-
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+connectDB().then(
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  })
+);
